f0ceda3555ee2aa4264acb3945b4fe019af5178c
vanessa.cp <vanehom@hotmail.com>
2023-03-29 09:38:18 +0200

    inicio

---
+++ src/test/java/com/acenturef5/Katas/KatasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acenturef5.Katas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
